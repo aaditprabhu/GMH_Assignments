e9a596feedae4b6cfe27ee711627fb851bb28cd2
aaditprabhu <aaditprabhu@gmail.com>
2021-02-20 17:20:53 -0500

    Assignment 1 Part 2 Completed

---
+++ Assignment1b.java
@@ -0,0 +1,21 @@
+import jdk.nashorn.internal.parser.Scanner;
+
+/**
+ * Assignment 1 Part b (Factorial)
+ */
+
+import java.util.*;
+
+public class Assignment1b {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number to Calculate the factorial:");
+        int num = scan.nextInt();
+        int fact = 1;
+        for(int i=1; i<=num; i++){
+            fact = fact*i;
+        }
+        System.out.println("The Factorial of "+ num +" is: " +fact);
+    }
+}
